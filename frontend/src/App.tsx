f75b0fca0cfe3488bdb9f001fd59d9c44b0b55b9
Nimna Pathum <113501913+nimnapathum@users.noreply.github.com>
2025-06-29 08:03:48 +0530

    Complete the footer section for home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
+import Footer from './components/FooterFixed';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import Signup from './pages/Signup';
@@ -14,21 +15,26 @@ const App: React.FC = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NewHome />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/i18n-example" element={<ExampleI18nPage />} />
-          <Route
-            path="/dashboard/*"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <div className="min-h-screen flex flex-col">
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<NewHome />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/i18n-example" element={<ExampleI18nPage />} />
+              <Route
+                path="/dashboard/*"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
       </BrowserRouter>
     </AuthProvider>
   );

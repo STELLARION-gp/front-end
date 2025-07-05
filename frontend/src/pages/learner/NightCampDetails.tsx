12c301c4d68151d4bb9975ba5ad114bd7932a90c
irumi <irumiabeywickrama@gmail.com>
2025-07-05 23:17:53 +0530

    [STEL-67]CREATE:create new page for night camp details

---
+++ frontend/src/pages/learner/NightCampDetails.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class NightCampDetails extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Night Camp Details</h2>
+                <p>Details about the selected night camp will be displayed here.</p>
+            </div>
+        );
+    }
+}
+
+export default NightCampDetails;

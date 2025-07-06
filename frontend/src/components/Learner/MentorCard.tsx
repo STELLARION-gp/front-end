3471d86ba852a153c922ef68046671b00efcec91
irumi <irumiabeywickrama@gmail.com>
2025-07-06 19:51:37 +0530

    [STEL-70]MODIFY : change mentor card style

--- frontend/src/components/Learner/MentorCard.tsx
+++ frontend/src/components/Learner/MentorCard.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import "../../styles/components/learner/MentorCard.scss";
+import Button from "../Button";
 
 interface MentorCardProps {
   mentor: {
     id: number;
     name: string;
     expertise: string;
+    description: string;
     availableSlots: number;
     image?: string;
+    accepting?: boolean;
   };
   onApply: (mentorId: number) => void;
 }
 
 const MentorCard: React.FC<MentorCardProps> = ({ mentor, onApply }) => {
   return (
-    <div className="mentor-card">
-      {mentor.image && <img src={mentor.image} alt={mentor.name} className="mentor-card__image" />}
+    <div className="mentor-card mentor-card--modern">
+      <div className="mentor-card__avatar-bg">
+        {mentor.image && (
+          <img src={mentor.image} alt={mentor.name} className="mentor-card__image" />
+        )}
+      </div>
       <div className="mentor-card__info">
         <h2 className="mentor-card__name">{mentor.name}</h2>
-        <p className="mentor-card__expertise">Expertise: {mentor.expertise}</p>
-        <p className="mentor-card__slots">Available Slots: {mentor.availableSlots}</p>
-        <button className="mentor-card__apply-btn" onClick={() => onApply(mentor.id)}>
-          Apply
-        </button>
+        <div className="mentor-card__expertise"><b>{mentor.expertise}</b></div>
+        <div className="mentor-card__desc">{mentor.description}</div>
+        <Button onClick={() => onApply(mentor.id)}>
+          Apply for Mentorship
+        </Button>
+        {mentor.accepting !== false && (
+          <div className="mentor-card__accepting">Currently Accepting Mentees</div>
+        )}
       </div>
     </div>
   );

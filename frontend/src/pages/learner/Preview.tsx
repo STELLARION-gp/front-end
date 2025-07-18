5f46b9e2eb92c60e8f7ee4c0e15346c6ba891f7c
irumi <irumiabeywickrama@gmail.com>
2025-07-18 17:18:13 +0530

    add one more event

--- frontend/src/pages/learner/Preview.tsx
+++ frontend/src/pages/learner/Preview.tsx
@@ -166,7 +166,7 @@ const organizedEvents = [
     contact: "camp@astro.lk",
     attendees: 80,
     description: "Enjoy the stars in a full-night observation camp with astronomers.",
-    sponsors: ["Celestia Society", "AstroLens"]
+    sponsors: ["Celestia Society"]
   },
   {
     id: 3,
@@ -178,7 +178,19 @@ const organizedEvents = [
     contact: "astro@galaxyfest.org",
     attendees: 500,
     description: "Embark on a breathtaking expedition across galaxies at Galactic Odyssey! Witness live telescope demos, space talks from top scientists, VR exploration zones, and interact with Sri Lanka’s top astronomy clubs. A cosmic experience for stargazers and dreamers alike.",
-    sponsors: ["NASA", "SpaceX", "Astro Lanka"]
+    sponsors: ["NASA", "Astro Lanka"]
+  },
+  {
+    id: 4,
+    name: "Stellar Science Fair",
+    category: "Astronomy",
+    imageUrl: "https://c8.alamy.com/comp/2CGXY86/stellar-nebula-and-cosmic-dust-cosmic-gas-clusters-and-constellations-in-deep-space-ideal-for-a-space-science-project-elements-furnished-by-nasa-2CGXY86.jpg",
+    date: "2025-09-21T19:00:00",
+    location: "National Planetarium, Colombo",
+    contact: "astro@galaxyfest.org",
+    attendees: 500,
+    description: "Embark on a breathtaking expedition across galaxies at Galactic Odyssey! Witness live telescope demos, space talks from top scientists, VR exploration zones, and interact with Sri Lanka’s top astronomy clubs. A cosmic experience for stargazers and dreamers alike.",
+    sponsors: ["NASA", "SpaceX"]
   }
 ];
 const Preview = () => {

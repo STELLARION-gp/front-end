0448bad1b707a8f9add4ca27c0a0c43d5efa3123
Sasanka Wakista <sasankasavindi02@gmail.com>
2025-07-19 23:20:46 +0530

    Update myblogs.tsx

--- frontend/src/pages/influencer/myblogs.tsx
+++ frontend/src/pages/influencer/myblogs.tsx
@@ -178,7 +178,7 @@ export default function MyBlogs() {
     });
 
     useEffect(() => {
-    const myBlogsOnly = blogs.filter(blog => blog.author === 'Neil V. Galaxy');
+    const myBlogsOnly = blogs.filter(blog => blog.author === 'You');
     setMyBlogs(myBlogsOnly);
 }, []);
 
@@ -216,7 +216,7 @@ export default function MyBlogs() {
             title: newBlog.title,
             content: newBlog.content,
             id: newId,
-            author: 'Neil V. Galaxy',
+            author: 'You',
             date: currentDate,
             createdAt: currentDate,
             reach: 0,
